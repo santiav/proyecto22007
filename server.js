05aae2fb16878e71898cdba5431710f5dcf0b8f3
Santi Verrier <santiav@gmail.com>
2022-06-30 16:20:34 -0300

    Router y partials agregados

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const app = express()
 const hbs = require('hbs')
-let nodemailer = require('nodemailer');
-
+var path = require('path');
 require('./helpers/helper');
-const datos = require('./productos.json')
-
 
 // Para que tome los datos de los formularios
 app.use(express.json());
@@ -13,64 +10,25 @@ app.use(express.urlencoded({
   extended: false
 }));
 
+app.use(express.static('public'));
+
+// HBS
 app.set('view engine', 'hbs');
+app.set('views', [
+    path.join('./views/front'),
+    path.join('./views/back'),
+    path.join('./views'),
+])
+hbs.registerPartials(__dirname + '/views/partials');
 
-// ruta raíz
-app.get('/', function (req, res) {
-    //console.log(datos[0].data)
-    res.render('index', {
-        productos: datos[0].data
-    })
-})
 
-// contacto
-app.get('/contacto', function (req, res) {
-    res.render('contacto')
-})
+// espacio para cargar el archivo de las rutas
+app.use('/', require('./routes/rutas'))
 
-app.post('/contacto', function(req, res) {
-    // 1. Definir el transportador
-    let transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: "9e67f1cd60d6f3",
-          pass: "086c522eabf980"
-        }
-    });
-    // 2. Definimos el cuerpo de mail
-    console.log("BODY: ", req.body)
-    let data = req.body
-    let mailOptions = {
-        from: data.nombre,
-        to: 'santiago.acosta@bue.edu.ar',
-        subject: data.asunto,
-        html: `
-            <h2>El siguiente mensaje ha llegado de la web</h2>
-            <p>${data.mensaje}</p>
-        `
-    }
-    // 3. Enviamos el mail
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.log(error)
-            res.status(500, error.message)
-            res.status(500).render('contacto', {
-                mensaje: `Ha ocurrido el siguiente error: ${error.message}`,
-                mostrar: true,
-                clase: 'danger'
-
-            })
-        } else {
-            console.log("E-mail enviado")
-            res.status(200).render('contacto', {
-                mensaje: `Tu e-mail ha sido enviado correctamente`,
-                mostrar: true,
-                clase: 'success'
-            })
-        }
-    })
-})
+// 404 - no encontrado
+app.use(function(req, res) {
+    res.status(404).render('404');
+});
 
 
 app.listen(3000, function () {
